d58eb34ba850f313e1a7528453ba5d562c2fae43
Michel <michel@michel-laptop.(none)>
2010-01-14 23:45:41 +0100

    Added test for defcontroller

--- src/org/clojuremvc/test/clojuremvc_test.clj
+++ src/org/clojuremvc/test/clojuremvc_test.clj
@@ -1,7 +1,26 @@
 (ns org.clojuremvc.test.clojuremvc-test
   (:gen-class)
   (:use clojure.contrib.test-is
-    org.clojuremvc.test.string-util-test))
+    org.clojuremvc
+    org.clojuremvc.test.string-util-test)
+   (:import
+    [org.springframework.web.servlet ModelAndView]
+    [org.springframework.web.servlet.mvc Controller]
+    [org.springframework.mock.web MockHttpServletRequest MockHttpServletResponse]))
+
+(defcontroller org.clojuremvc.test.TestController
+  "/index.html"
+  [:index {:message "Welcome to Clojure MVC."}])
+
+(deftest defcontroller-test
+  (let [testController (org.clojuremvc.test.TestController.)]
+    (let [action (.get testController "/index.html")]
+      (let [mav (.handleRequest action 
+                  (MockHttpServletRequest. "POST" "/index.html")
+                  (MockHttpServletResponse.))]
+        (is (instance? ModelAndView mav))
+        (is (= "index" (.getViewName mav)))
+        (is (= {"message" "Welcome to Clojure MVC."} (.getModel mav)))))))
 
 (defn -main []
   (run-all-tests #"org.clojuremvc.test.*"))
