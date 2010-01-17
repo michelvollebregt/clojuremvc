a444b356a9eb38424be34ad088f4a2f8484f318a
Michel <michel@michel-laptop.(none)>
2010-01-17 19:33:01 +0100

    Changed syntax of defcontroller macro. Added [] for parameters to each action, and each action is now put between ().

--- src/org/clojuremvc.clj
+++ src/org/clojuremvc.clj
@@ -9,17 +9,21 @@
         shortname (strip (str classname) #(inc (.lastIndexOf % ".")))]
     `(do
        (defn ~(symbol (str "-" shortname "-postinit")) [~this]
-         ~@(for [[url handler] (apply hash-map body)]
+         ~@(for [[url params & handler] body]
              `(.put ~this ~url
                  (proxy [AbstractController] []
                    (handleRequestInternal [request# response#]
-                     (let [[view# model#] ~handler]
+                     (let [[view# model#] (do ~@handler)]
                        (ModelAndView. (tostr view#) (keys-to-strings model#))))))))
        (gen-class
          :name ~classname
          :extends java.util.HashMap
          :post-init ~(str shortname "-postinit")
          ))))
+
+;;; kunnen we ook defcontroller een methode laten definieren die we annoteren
+;;; met metadata, zodat defmapping deze kan terugvinden?
+;;; vraag: kunnen we ALLE namespaceas doorzoeken? (hoe gaat dit in test-is?)
 
 (defmacro defmapping [classname & body]
   (let [this (gensym "this")
--- src/org/clojuremvc/test/clojuremvc_test.clj
+++ src/org/clojuremvc/test/clojuremvc_test.clj
@@ -2,6 +2,8 @@
   (:gen-class)
   (:use clojure.contrib.test-is
     org.clojuremvc
+    org.clojuremvc.test.params-test
+    org.clojuremvc.test.collect-map-test
     org.clojuremvc.test.string-util-test)
    (:import
     [org.springframework.web.servlet ModelAndView]
@@ -9,18 +11,31 @@
     [org.springframework.mock.web MockHttpServletRequest MockHttpServletResponse]))
 
 (defcontroller org.clojuremvc.test.TestController
-  "/index.html"
-  [:index {:message "Welcome to Clojure MVC."}])
+  ("/index.html" []
+    [:index {:message "Welcome to Clojure MVC."}])
+  ("/sideeffects.html" []
+    (println "side effect")
+    [:index {:remark "This controller had a side effect."}]))
 
-(deftest defcontroller-test
-  (let [testController (org.clojuremvc.test.TestController.)]
-    (let [action (.get testController "/index.html")]
-      (let [mav (.handleRequest action 
-                  (MockHttpServletRequest. "POST" "/index.html")
-                  (MockHttpServletResponse.))]
-        (is (instance? ModelAndView mav))
-        (is (= "index" (.getViewName mav)))
-        (is (= {"message" "Welcome to Clojure MVC."} (.getModel mav)))))))
+(let [testController (org.clojuremvc.test.TestController.)]
+      
+  (deftest defcontroller-test
+    (let [action (.get testController "/index.html")
+          mav (.handleRequest action
+                (MockHttpServletRequest. "POST" "/index.html")
+                (MockHttpServletResponse.))]
+    (is (instance? ModelAndView mav))
+    (is (= "index" (.getViewName mav)))
+    (is (= {"message" "Welcome to Clojure MVC."} (.getModel mav)))))
+
+  (deftest side-effect-test
+    (let [action (.get testController "/sideeffects.html")
+          mav (.handleRequest action
+                (MockHttpServletRequest. "GET" "/sideeffects.html")
+                (MockHttpServletResponse.))]
+      (is (instance? ModelAndView mav))
+      (is (= "index" (.getViewName mav)))
+      (is (= {"remark" "This controller had a side effect."} (.getModel mav))))))
 
 (defn -main []
   (run-all-tests #"org.clojuremvc.test.*"))
